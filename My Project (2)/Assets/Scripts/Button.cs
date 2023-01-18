6cc50378184ce4966dde21fd4b307506a968e498
ilginarpaci@gmail.com <88985657+Ilgin-a@users.noreply.github.com>
2023-01-18 16:37:54 +0300

    First commit

---
+++ My Project (2)/Assets/Scripts/Button.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+public class Button : MonoBehaviour
+{
+    public bool isHit;
+
+    public Counter counter;
+
+    [SerializeField] private AudioSource successSoundEffect;
+    [SerializeField] private AudioSource missedSoundEffect;
+    // Start is called before the first frame update
+    public void OnButtonClick()
+    {
+        if ((Mathf.Abs(Player.Instance.transform.rotation.eulerAngles.x % 90) <= 10) && isHit)
+        {
+            successSoundEffect.Play();
+            StartCoroutine(ResetHit());
+            counter.IncreaseCounter();
+
+        }
+        else
+        {
+            missedSoundEffect.Play();
+        }
+
+    }
+
+    public IEnumerator ResetHit()
+    {
+        
+        isHit = false;
+        yield return new WaitForSeconds(0.5f);
+        isHit = true;
+    }
+}
+
